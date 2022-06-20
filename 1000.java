2e2ea61afbb68831482ab3c9da3ed0a521ec1f9f
Orlando André Souza Neto <orlandoandre019@gmail.com>
2022-06-20 09:57:44 -0300

    Hello World

---
+++ 1000.java
@@ -0,0 +1,23 @@
+/*@autor: Orlando André 
+ * 
+ *@name: HELLO WORLD
+*/
+
+/*Bem-vindo ao beecrowd!
+O seu primeiro programa em qualquer linguagem de programação normalmente é o "Hello World!". Neste primeiro problema tudo o que você precisa fazer é imprimir esta mensagem na tela.
+
+Entrada
+Este problema não possui nenhuma entrada.
+
+Saída
+Você deve imprimir a mensagem "Hello World!" e em seguida o final de linha, conforme o exemplo abaixo*/
+
+import java.io.IOException;
+
+public class 1000 { //P.S colocar Main 
+ 
+    public static void main(String[] args) throws IOException {
+    System.out.println("Hello World!");
+    }
+ 
+}
