824f28119cd8e795bed480de27b6f5f844a08003
Orlando André Souza Neto <orlandoandre019@gmail.com>
2022-06-21 09:47:00 -0300

    Área do círculo

---
+++ 1002.java
@@ -0,0 +1,29 @@
+/*@autor: Orlando André 
+ * 
+ *@name: Extremamente Básico
+*/
+/* 
+A fórmula para calcular a área de uma circunferência é: area = π . raio2. Considerando para este problema que π = 3.14159:
+
+- Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.
+
+Entrada
+A entrada contém um valor de ponto flutuante (dupla precisão), no caso, a variável raio.
+
+Saída
+Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo abaixo, com 4 casas após o ponto decimal. Utilize variáveis de dupla precisão (double). Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".*/
+
+import java.util.Locale;
+import java.util.Scanner;
+public class 1002 { //P.S colocar Main 
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner input = new java.util.Scanner(System.in);
+        double raio, area, pi;
+        pi =3.14159;
+        raio = input.nextDouble();
+        area = Math.pow(raio,2) *pi;
+        System.out.printf("A=%.4f\n", area);
+        input.close();
+    }
+}
