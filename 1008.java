e780992444db5d9575fa91326b076112eb38e2ba
Orlando André Souza Neto <orlandoandre019@gmail.com>
2022-06-22 18:14:22 -0300

    Salário

--- 1008.java
+++ 1008.java
@@ -1,3 +1,29 @@
-public class 1008 {
-    
+/*@autor: Orlando André 
+ * 
+ *@name: Salário
+*/
+/*Escreva um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
+
+Entrada
+O arquivo de entrada contém 2 números inteiros e 1 número com duas casas decimais, representando o número, quantidade de horas trabalhadas e o valor que o funcionário recebe por hora trabalhada, respectivamente.
+
+Saída
+Imprima o número e o salário do funcionário, conforme exemplo fornecido, com um espaço em branco antes e depois da igualdade. No caso do salário, também deve haver um espaço em branco após o $. */
+
+import java.util.Locale;
+import java.util.Scanner;
+public class 1008 {//P.S colocar Main
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner input = new java.util.Scanner(System.in); 
+        int N, qtdHT;
+        double VH, SALARY;
+        N = input.nextInt();
+        qtdHT = input.nextInt();
+        VH = input.nextDouble();
+        SALARY = VH * qtdHT;
+        System.out.println("NUMBER = " + N);
+        System.out.printf("SALARY = U$ %.2f\n" , SALARY);
+        input.close();
+    }
 }
