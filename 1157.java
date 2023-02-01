96f40892643a81e2e9cb8f3aad3d3fe28d312e20
Orlando André Souza Neto <orlandoandre019@gmail.com>
2023-02-01 10:51:39 -0300

    Exercício 1157

---
+++ 1157.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner input = new Scanner(System.in);
+        int n;
+        n = input.nextInt();
+        for(int i = 1; i <= n ; i++){
+            if(n % i == 0){
+                System.out.println(i);
+            }
+        }
+        input.close();
+    }
+ 
+}
