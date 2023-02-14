7fb73be64a3e12f2a2f1cb74f44109c7fe78f8c7
Orlando André Souza Neto <orlandoandre019@gmail.com>
2023-02-14 09:32:14 -0300

    Create 1190.java

---
+++ 1190.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new java.util.Scanner(System.in);
+        double [][] A = new double [12][12];
+        double soma = 0.0, media = 0.0;
+        double cont = 0.0;
+        char operação;
+        int x = 0;
+        operação = input.next().charAt(0);
+        for(int i = 0 ; i < 12; i++){
+            for(int j = 0 ; j < 12 ; j++){
+                A[i][j] = input.nextDouble();
+            }
+        }
+        for(int j = 11 ; j > 6 ; j--){
+            x++;
+            for(int i = x ; i < 12 - x; i++){
+                soma = soma + A[i][j];
+                cont++;
+            }
+        }
+        if(operação == 'S'){
+            System.out.printf("%.1f\n",soma );
+        }
+        else{
+            media =soma/cont;
+            System.out.printf("%.1f\n", media);
+        }
+        input.close();
+    }
+}
+
